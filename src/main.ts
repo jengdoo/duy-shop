c915def55ff3e64401bb27568922c939b6504505
Jengdoo <macdinhduy28052004@gmail.com>
2024-09-11 00:00:23 +0700

    configuration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import { HomeComponent } from './app/home/home.component';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(HomeComponent, appConfig).catch((err) =>
+  console.error(err)
+);
