3fc7de60cdf0dc1f22f733558873a7c978b149e2
Jengdoo <macdinhduy28052004@gmail.com>
2024-09-20 05:31:44 +0700

    Đã xóa tất cả dữ liệu và thêm dữ liệu mới

--- backend/shopapp/src/main/java/com/example/shopapp/exception/DataNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.shopapp.exception;
-
-public class DataNotFoundException extends Exception{
-    public DataNotFoundException(String message){
-        super(message);
-    }
-}
--- backend/shopapp/src/main/java/com/example/shopapp/exception/InvalidParamException.java
+++
@@ -1,7 +0,0 @@
-package com.example.shopapp.exception;
-
-public class InvalidParamException extends Exception{
-    public InvalidParamException(String message){
-        super(message);
-    }
-}
